7a1d112d4541a7ff8d13a94b8ff2ef92c8656679
Gvinu <Gvinu@Vinusha_Mahesh>
2026-01-17 13:15:32 +0530

    added method

--- ComcastCRMGUIFrameworkWithTestNG/src/main/java/com/comcast/crm/conflict/Conflict.java
+++ ComcastCRMGUIFrameworkWithTestNG/src/main/java/com/comcast/crm/conflict/Conflict.java
@@ -1,5 +1,12 @@
 package com.comcast.crm.conflict;
 
+import org.testng.annotations.Test;
+
 public class Conflict {
+	@Test
+	public void conflictMethod()
+	{
+		System.out.println("conflict");
+	}
 
 }
